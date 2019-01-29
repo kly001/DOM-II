56488d8c514eecc6608acc81b76db5a7ee6df63f
Preprocessing - I <karenly1961@gmail.com>
2019-01-29 17:47:06 -0500

    Click, Mouseleave Events

--- js/index.js
+++ js/index.js
@@ -4,14 +4,24 @@ headLogo.addEventListener('mouseover', e =>{
     alert('This is the FunBus Heading')
 })
 
-
-
 const letsGo= document.querySelector('.content-section h2');
 letsGo.addEventListener('dblclick', e => {
-    letsGo.style.backgroundColor = 'magenta';
+    if(letsGo.style.backgroundColor='magenta');
 })
 
 const funbusImage=document.querySelector('.intro img');
 funbusImage.addEventListener('mouseenter',e =>{
-    funbusImage.style.width="500px";
+    funbusImage.style.width='500px';
+})
+
+ const welcome = document.querySelector('.intro h2');
+ welcome.addEventListener('click', e => {
+     welcome.style.fontSize ='10rem';
+ })
+
+
+const adventure = document.querySelector('.text-content p');
+adventure.addEventListener('mouseleave', e => {
+    adventure.style.color = 'purple';
+    adventure.style.fontWeight = 'bold';
 })
