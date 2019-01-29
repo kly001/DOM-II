ccfd8f6432f8b2c951aac9ed7bb0986355128bdf
Preprocessing - I <karenly1961@gmail.com>
2019-01-29 16:33:50 -0500

    Mouseover, Mouseenter, Dblclick Events

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 // Your code goes here
-new
+const headLogo = document.querySelector('.logo-heading');
+headLogo.addEventListener('mouseover', e =>{
+    alert('This is the FunBus Heading')
+})
+
+
+
+const letsGo= document.querySelector('.content-section h2');
+letsGo.addEventListener('dblclick', e => {
+    letsGo.style.backgroundColor = 'magenta';
+})
+
+const funbusImage=document.querySelector('.intro img');
+funbusImage.addEventListener('mouseenter',e =>{
+    funbusImage.style.width="500px";
+})
